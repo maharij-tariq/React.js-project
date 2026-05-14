61b9d8ad404c654f073a5677e26e53b20543471b
MFaad1 <faadsardar123@gmail.com>
2026-05-14 23:15:35 +0500

    Calculator porject React.js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Calculator from './calculator/Calculator'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center w-full  h-[100vh]' >
+      <Calculator />
+    </div>
+  )
+}
+
+export default App
